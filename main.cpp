cd947870c812baff8c72ab50157102782457b41b
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-03-29 13:55:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   float a,b;
+   float x,y;
+   cin >> a>>b;
+   cin >> x>> y;
+   if( ( x<0, y>0){
+       cout << (a*x-b*y)<< endl;
+   }
+   else if (x>=0, y<=0){
+       cout << (a*x*x-b*y)<< endl;
+   }
+   else{
+       cout << (a*x +b*y*y)<< endl;
+   }
    return 0;
 }
