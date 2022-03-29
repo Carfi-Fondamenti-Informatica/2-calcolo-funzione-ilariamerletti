6c86c5413d5ad3db5b986646fffd4aa193a1d164
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-03-29 14:18:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ int main() {
    cin >> a>>b;
    cin >> x>> y;
    float z;
-   if ( x<0, y>0){
+   if ( x<0 && y>0){
        z=(a*x-b*y);
       cout << (z) << endl;
    }
-   else if (x>=0, y<=0){
+   else if (x>=0 && y<=0){
      z=(a*(x*x)-b*y);
       cout << (z) << endl;
    }
